40896e8e462fad3f18398b49dd7ef84e84465898
nishii118 <poro2420033@gmail.com>
2024-01-13 00:17:10 +0700

    new feature: increase decrease remove product

--- src/components/ItemInCart.jsx
+++ src/components/ItemInCart.jsx
@@ -1,43 +1,58 @@
-import React from 'react'
-import "../assets/styles/ItemInCart.css"
+import React from "react";
+import "../assets/styles/ItemInCart.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../store/cartSlicer";
 
-const ItemIncart = ({item}) => {
-  let sum = item.price*item.quantity
+const ItemIncart = ({ item }) => {
+  const dispatch = useDispatch();
+
+  let sum = item.price * item.quantity;
   return (
-    <div className='menu-cart-item' id={item.id}>
-        <div className="product-quantity">
-            <button className='quantity-btn quantity-btn'>
-                <i className='fa-solid fa-plus'></i>
-            </button>
-            <span>{item.quantity}</span>
-            <button className='quantity-btn quantity-btn'>
-                <i className='fa-solid fa-minus'></i>
-            </button>
+    <div className="menu-cart-item" id={item.id}>
+      <div className="product-quantity">
+        <button
+          className="quantity-btn quantity-btn"
+          onClick={() => {
+            dispatch(cartActions.increaseProduct(item));
+          }}
+        >
+          <i className="fa-solid fa-plus"></i>
+        </button>
+        <span>{item.quantity}</span>
+        <button
+          className="quantity-btn quantity-btn"
+          onClick={() => {
+            dispatch(cartActions.decreaseProduct(item));
+          }}
+        >
+          <i className="fa-solid fa-minus"></i>
+        </button>
+      </div>
 
+      <div className="product-info">
+        <div
+          className="product-img"
+          style={{
+            backgroundImage: `url(http://103.252.95.181:8000${item.images})`,
+          }}
+        ></div>
+
+        <div className="product-description">
+          <div className="product-name">{item.productName}</div>
+          <div className="product-price">{`${item.price}đ x ${item.quantity}`}</div>
+          <div className="product-total-price">{`${sum}đ`}</div>
         </div>
-
-        <div className="product-info">
-            
-            <div
-            className="product-img"
-            style={{
-              backgroundImage: `url(http://103.252.95.181:8000${item.images})`,
-            
-            }}
-          ></div>
-
-
-            <div className="product-description">
-                <div className="product-name">{item.productName}</div>
-                <div className="product-price">{`${item.price}đ x ${item.quantity}`}</div>
-                <div className="product-total-price">{`${sum}đ`}</div>
-            </div>
-        </div>
-        <div className="product-button-close">
-            <i className='fa-solid fa-xmark btnclose'></i>
-        </div>
+      </div>
+      <div
+        className="product-button-close"
+        onClick={() => {
+          dispatch(cartActions.removeProduct(item));
+        }}
+      >
+        <i className="fa-solid fa-xmark btnclose"></i>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ItemIncart
+export default ItemIncart;
--- src/store/cartslicer.jsx
+++ src/store/cartslicer.jsx
@@ -11,12 +11,16 @@ const cartSlicer = createSlice({
     addToCart(state, action) {
       const payload = action.payload;
       console.log(payload);
-      const existingProduct = state.cart.find(item => item.id === payload.id);
+      const existingProduct = state.cart.find((item) => item.id === payload.id);
       console.log(state.cart);
 
       if (existingProduct != null) {
-        let getIndex = state.cart.findIndex((product) => product.id === existingProduct.id);
-        let getProduct = state.cart.find((product) => product.id === existingProduct.id);
+        let getIndex = state.cart.findIndex(
+          (product) => product.id === existingProduct.id
+        );
+        let getProduct = state.cart.find(
+          (product) => product.id === existingProduct.id
+        );
         let updatedQuantity = getProduct.quantity + 1;
         state.cart[getIndex] = {
           ...getProduct,
@@ -25,13 +29,52 @@ const cartSlicer = createSlice({
       } else {
         state.totalTypeProductInCart += 1;
         state.cart.push({
-          ...payload
-        })
+          ...payload,
+        });
       }
       console.log(state.totalTypeProductInCart);
+    },
+
+    increaseProduct(state, action) {
+      const payload = action.payload;
+      const existingProduct = state.cart.find((item) => item.id === payload.id);
+      let getIndex = state.cart.findIndex(
+        (product) => product.id === existingProduct.id
+      );
+      let getProduct = state.cart.find(
+        (product) => product.id === existingProduct.id
+      );
+      let updatedQuantity = getProduct.quantity + 1;
+      state.cart[getIndex] = {
+        ...getProduct,
+        quantity: updatedQuantity,
+      };
+    },
+
+    decreaseProduct(state, action) {
+      const payload = action.payload;
+      const existingProduct = state.cart.find((item) => item.id === payload.id);
+      let getIndex = state.cart.findIndex(
+        (product) => product.id === existingProduct.id
+      );
+      let getProduct = state.cart.find(
+        (product) => product.id === existingProduct.id
+      );
+      let updatedQuantity = 1;
+      if (getProduct.quantity > 1) {
+        updatedQuantity = getProduct.quantity - 1;
+      }
+      state.cart[getIndex] = {
+        ...getProduct,
+        quantity: updatedQuantity,
+      };
+    },
+
+    removeProduct(state, action) {
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
     }
-  }
-})
+  },
+});
 
 export const cartActions = cartSlicer.actions;
 export default cartSlicer.reducer;
