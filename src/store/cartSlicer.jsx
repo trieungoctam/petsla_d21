69ae8962c31ec8625c7fe27bcef8f85a53637f7b
phuongwthao <phuongthaoluu2401@gmail.com>
2024-01-14 00:18:10 +0700

    Moneytotal must >0

--- src/store/cartSlicer.jsx
+++ src/store/cartSlicer.jsx
@@ -75,7 +75,10 @@ const cartSlicer = createSlice({
         ...getProduct,
         quantity: updatedQuantity,
       };
-      state.totalMoney -= getProduct.price;
+      if(state.totalMoney>0){
+        state.totalMoney -= getProduct.price;
+      }
+     
     },
 
     removeProduct(state, action) {
